e77d74cae19f48d7258e6d452b4c18b17d3f983e
onisuly <onisuly@gmail.com>
2018-07-17 22:25:23 +0800

    Update start shell script.

--- files/start.sh
+++ files/start.sh
@@ -4,15 +4,17 @@ set -e
 if [ ! -f /conf/aria2.conf ]; then
     cp /preset-conf/aria2.conf /conf/aria2.conf
     if [ $SECRET ]; then
-        echo "rpc-secret=${SECRET}\n" >> /conf/aria2.conf
+        echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
     fi
 
     if [ $SECURE = 'true' ]; then
-        echo "rpc-secure=true\n" >> /conf/aria2.conf
-        echo "rpc-certificate=$CERTIFICATE\n" >> /conf/aria2.conf
-        echo "rpc-private-key=$PRIVATEKEY\n" >> /conf/aria2.conf
+        echo "" >> /conf/aria2.conf
+        echo "rpc-secure=true" >> /conf/aria2.conf
+        echo "rpc-certificate=$CERTIFICATE" >> /conf/aria2.conf
+        echo "rpc-private-key=$PRIVATEKEY" >> /conf/aria2.conf
     fi
 
+    echo "" >> /conf/aria2.conf
     list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
     if [ -z "`grep "bt-tracker" /conf/aria2.conf`" ]; then
         sed -i '$a bt-tracker='${list} /conf/aria2.conf
